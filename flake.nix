55f7131ad34b465aa863f47702e64d64a8fdbb62
Mihai Fufezan <fufexan@pm.me>
2021-05-19 19:04:38 +0300

    flake: expose native overlay

--- flake.nix
+++ flake.nix
@@ -7,23 +7,26 @@
   };
 
   outputs = { self, nixpkgs, utils }:
-    utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ] (
-      system:
-        let
-          overlay = final: prev: {
-            wine-osu = prev.callPackage ./wine-osu.nix {};
-          };
+    let
+      # expose overlay outside of fu so it doesn't get output as overlay.${system}
+      overlay = final: prev: {
+        wine-osu = prev.callPackage ./wine-osu.nix {};
+      };
+    in
+      # only x86 linux is supported by wine
+      utils.lib.eachSystem [ "i686-linux" "x86_64-linux" ] (
+        system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ overlay ];
+            };
 
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ overlay ];
-          };
-
-          packages = { inherit (pkgs) wine-osu; };
-        in
-          {
-            inherit packages overlay;
-            defaultPackage = packages.wine-osu;
-          }
-    );
+            packages = { inherit (pkgs) wine-osu; };
+          in
+            {
+              inherit packages;
+              defaultPackage = packages.wine-osu;
+            }
+      ) // { inherit overlay; };
 }
