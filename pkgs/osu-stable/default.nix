696663406861621bf6fb6f8dcf319571cac947d4
Mihai Fufezan <fufexan@pm.me>
2021-08-29 23:25:13 +0300

    Revert "osu-stable: update installer hash *yet again*"
    This reverts commit f2bc4e685483baebe7e637041958e6b8f54538b3.

--- pkgs/osu-stable/default.nix
+++ pkgs/osu-stable/default.nix
@@ -17,7 +17,7 @@ let
   src = builtins.fetchurl {
     url = "https://m1.ppy.sh/r/osu!install.exe";
     name = "osuinstall.exe";
-    sha256 = "18rhzq2ddfnf7iqp5k2fai1jikb8bs8h97xzp2yymgqg38akzgqa";
+    sha256 = "sha256-Ibzs3JsDlvB1qtT+EvSsnZN6Yike5vYzdXoxAASQcWs=";
   };
   icon = builtins.fetchurl {
     url = "https://i.ppy.sh/013ed2c11b34720790e74035d9f49078d5e9aa64/68747470733a2f2f6f73752e7070792e73682f77696b692f696d616765732f4272616e645f6964656e746974795f67756964656c696e65732f696d672f75736167652d66756c6c2d636f6c6f75722e706e67";
