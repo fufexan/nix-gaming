b5a4286e74090adbabf6b6ad1d521423b9f9cadd
Mihai Fufezan <fufexan@pm.me>
2022-01-06 14:55:11 +0200

    flake.nix: add nixos substituter

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
   };
 
   nixConfig = {
-    substituters = [ "https://nix-gaming.cachix.org" ];
-    trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+    substituters = [ "https://cache.nixos.org" "https://nix-gaming.cachix.org" ];
+    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
   };
 
   outputs = { self, nixpkgs, utils, ... }@inputs:
