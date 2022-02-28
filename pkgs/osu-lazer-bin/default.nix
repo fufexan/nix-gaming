ab3e625ab025933ffdf9070358aaafaa34994f44
Mihai Fufezan <fufexan@pm.me>
2022-02-28 03:13:36 +0200

    osu-lazer-bin: add options for gmrun and pipewire latency
    The gmrun can be toggled with the option, and the pipewire latency is
    configurable.
    
    osu-lazer-bin: update

--- pkgs/osu-lazer-bin/default.nix
+++ pkgs/osu-lazer-bin/default.nix
@@ -16,12 +16,14 @@
 , stdenvNoCC
 , symlinkJoin
 , writeShellScript
+, pipewire_latency ? "24/96000"
+, gmrun_enable ? true
 }:
 let
-  version = "2022.217.0";
+  version = "2022.226.0";
   appimageBin = fetchurl {
     url = "https://github.com/ppy/osu/releases/download/${version}/osu.AppImage";
-    sha256 = "sha256-RIDzIkAmdD47xMCR8xgBPDaC7WFZaJtrK45qqSxcN4k=";
+    sha256 = "sha256-UZZEubx6byADBJ1THcZL541l5n7TqoKgEgbchxMv/RY=";
   };
   extracted = appimageTools.extract {
     inherit version;
@@ -47,25 +49,26 @@ let
       makeWrapper
     ];
     autoPatchelfIgnoreMissingDeps = true;
-    installPhase = let
-      # dirty hack to infiltrate gamemoderun in the wrapper
-      gmrun = writeShellScript "gmrun" ''
-        shift
-        exec ${gamemode}/bin/gamemoderun "''$@"
+    installPhase =
+      let
+        # dirty hack to infiltrate gamemoderun in the wrapper
+        gmrun = writeShellScript "gmrun" ''
+          shift
+          exec ${gamemode}/bin/gamemoderun "''$@"
+        '';
+      in
+      ''
+        runHook preInstall
+        install -d $out/bin $out/lib
+        install osu\!.png $out/osu.png
+        cp -r usr/bin $out/lib/osu
+        makeWrapper $out/lib/osu/osu\! $out/bin/osu-lazer \
+          --set COMPlus_GCGen0MaxBudget "600000" \
+          --set PIPEWIRE_LATENCY "${pipewire_latency}" \
+          --set vblank_mode "0" \
+          --suffix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" ${if gmrun_enable then ''--run "${gmrun} \\"'' else ""}
+        runHook postInstall
       '';
-    in ''
-      runHook preInstall
-      install -d $out/bin $out/lib
-      install osu\!.png $out/osu.png
-      cp -r usr/bin $out/lib/osu
-      makeWrapper $out/lib/osu/osu\! $out/bin/osu-lazer \
-        --set COMPlus_GCGen0MaxBudget "600000" \
-        --set PIPEWIRE_LATENCY "24/96000" \
-        --set vblank_mode "0" \
-        --suffix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}" \
-        --run "${gmrun} \\"
-      runHook postInstall
-    '';
     fixupPhase = ''
       runHook preFixup
       ln -sft $out/lib/osu ${SDL2}/lib/libSDL2${stdenvNoCC.hostPlatform.extensions.sharedLibrary}
