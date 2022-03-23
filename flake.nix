46a15e94a0a65e21e1721ff8fda6b69e06fc6a41
Mihai Fufezan <fufexan@pm.me>
2022-03-23 14:43:46 +0200

    style updates

--- flake.nix
+++ flake.nix
@@ -3,16 +3,12 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  nixConfig = {
-    substituters = [ "https://cache.nixos.org" "https://nix-gaming.cachix.org" ];
-    trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
-  };
-
   outputs = { self, nixpkgs, ... }@inputs:
-    # only x86 linux is supported by wine
     let
+      # helper functions
       lib = import ./lib inputs;
 
+      # the rest of packages should work automatically
       apps = lib.forAllSystems (system: {
         osu-lazer = {
           program = packages.${system}.osu-lazer-bin.outPath + "/bin/osu-lazer";
@@ -20,6 +16,7 @@
         };
       });
 
+      # in case you want to add the packages to your pkgs
       overlays.default = final: prev: import ./pkgs {
         inherit inputs;
         pkgs = prev;
@@ -28,7 +25,10 @@
       packages = lib.forAllSystems (system:
         (import ./pkgs {
           inherit inputs;
-          pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
         }));
     in
     {
@@ -37,4 +37,17 @@
       nixosModules.pipewireLowLatency = import ./modules/pipewireLowLatency.nix;
       nixosModule = inputs.self.nixosModules.pipewireLowLatency;
     };
+
+  # auto-fetch wine when `nix run/shell`ing
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org"
+      "https://nix-gaming.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
+  };
 }
--- lib/default.nix
+++ lib/default.nix
@@ -3,15 +3,15 @@ inputs:
 let
   inherit (inputs.nixpkgs.lib) hasSuffix filesystem genAttrs;
 
-  lib = rec {
-    mkPatches = dir: map (e: /. + e)
-      (builtins.filter
-        (hasSuffix ".patch")
-        (filesystem.listFilesRecursive dir));
+  mkPatches = dir: map (e: /. + e)
+    (builtins.filter
+      (hasSuffix ".patch")
+      (filesystem.listFilesRecursive dir));
 
-    forAllSystems = genAttrs supportedSystems;
+  forAllSystems = genAttrs supportedSystems;
 
-    supportedSystems = [ "x86_64-linux" ];
-  };
+  supportedSystems = [ "x86_64-linux" ];
 in
-lib
+{
+  inherit mkPatches forAllSystems;
+}
