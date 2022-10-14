fb7e1ebe0eb44628c353e1d13923f602c8d8f784
Mihai Fufezan <fufexan@protonmail.com>
2022-10-14 20:58:10 +0300

    osu-stable: update sha

--- pkgs/osu-stable/default.nix
+++ pkgs/osu-stable/default.nix
@@ -17,7 +17,7 @@
   src = builtins.fetchurl rec {
     url = "https://m1.ppy.sh/r/osu!install.exe";
     name = "osuinstall-${sha256}.exe";
-    sha256 = "0wgsqwi8avcmvmw99fvpyybxs5bxg5vqylp1qikvmjwgyr09ik03";
+    sha256 = "15h31bzd4a76sz2l4xrzdga061irp1b7cai59zjwib25n811v0ri";
   };
   icon = builtins.fetchurl {
     url = "https://i.ppy.sh/013ed2c11b34720790e74035d9f49078d5e9aa64/68747470733a2f2f6f73752e7070792e73682f77696b692f696d616765732f4272616e645f6964656e746974795f67756964656c696e65732f696d672f75736167652d66756c6c2d636f6c6f75722e706e67";
