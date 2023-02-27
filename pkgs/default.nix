3096bce8c0e49e24cee1c7720df4c99d919663df
Mihai Fufezan <fufexan@protonmail.com>
2023-02-27 16:10:41 +0200

    flake: use flake-parts

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,62 +1,76 @@
 {
   inputs,
-  pkgs,
-}: let
-  inherit (pkgs) callPackage;
-  pins = import ../npins;
+  self,
+  ...
+}: {
+  systems = ["x86_64-linux"];
 
-  wineBuilder = wine: build: extra:
-    (import ./wine ({
-        inherit inputs build pkgs pins;
-        inherit (pkgs) callPackage fetchFromGitHub fetchurl lib autoconf perl moltenvk hexdump pkgsCross pkgsi686Linux stdenv_32bit;
-        supportFlags = (import ./wine/supportFlags.nix).${build};
-      }
-      // extra))
-    .${wine};
+  imports = [inputs.flake-parts.flakeModules.easyOverlay];
 
-  packages = rec {
-    dxvk = callPackage ./dxvk {inherit pins;};
-    dxvk-w32 = pkgs.pkgsCross.mingw32.callPackage ./dxvk {inherit pins;};
-    dxvk-w64 = pkgs.pkgsCross.mingwW64.callPackage ./dxvk {inherit pins;};
-
-    faf-client = callPackage ./faf-client {};
-    faf-client-unstable = callPackage ./faf-client {unstable = true;};
-
-    osu-lazer-bin = callPackage ./osu-lazer-bin {inherit pins;};
-
-    osu-stable = callPackage ./osu-stable {
-      wine = wine-osu;
-      wine-discord-ipc-bridge = wine-discord-ipc-bridge.override {wine = wine-osu;};
+  perSystem = {
+    config,
+    system,
+    pkgs,
+    ...
+  }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
     };
 
-    proton-ge = callPackage ./proton-ge {};
+    packages = let
+      pins = import ../npins;
 
-    roblox-player = callPackage ./roblox-player {
-      wine = wine-tkg;
-      inherit wine-discord-ipc-bridge;
+      wineBuilder = wine: build: extra:
+        (import ./wine ({
+            inherit inputs self pkgs build pins;
+            inherit (pkgs) callPackage fetchFromGitHub fetchurl lib moltenvk pkgsCross pkgsi686Linux stdenv_32bit;
+            supportFlags = (import ./wine/supportFlags.nix).${build};
+          }
+          // extra))
+        .${wine};
+    in {
+      dxvk = pkgs.callPackage ./dxvk {inherit pins;};
+      dxvk-w32 = pkgs.pkgsCross.mingw32.callPackage ./dxvk {inherit pins;};
+      dxvk-w64 = pkgs.pkgsCross.mingwW64.callPackage ./dxvk {inherit pins;};
+
+      faf-client = pkgs.callPackage ./faf-client {};
+      faf-client-unstable = pkgs.callPackage ./faf-client {unstable = true;};
+
+      osu-lazer-bin = pkgs.callPackage ./osu-lazer-bin {inherit pins;};
+
+      osu-stable = pkgs.callPackage ./osu-stable {
+        wine = config.packages.wine-osu;
+        wine-discord-ipc-bridge = config.packages.wine-discord-ipc-bridge.override {wine = config.packages.wine-osu;};
+      };
+
+      proton-ge = pkgs.callPackage ./proton-ge {};
+      roblox-player = pkgs.callPackage ./roblox-player {
+        wine = config.packages.wine-tkg;
+        inherit (config.packages) wine-discord-ipc-bridge;
+      };
+
+      technic-launcher = pkgs.callPackage ./technic-launcher {};
+
+      vkd3d-proton = pkgs.callPackage ./vkd3d-proton {inherit pins;};
+      vkd3d-proton-w32 = pkgs.pkgsCross.mingw32.callPackage ./vkd3d-proton {inherit pins;};
+      vkd3d-proton-w64 = pkgs.pkgsCross.mingwW64.callPackage ./vkd3d-proton {inherit pins;};
+
+      wine-discord-ipc-bridge = pkgs.callPackage ./wine-discord-ipc-bridge {
+        inherit pins;
+        wine = config.packages.wine-tkg;
+      };
+
+      # broken
+      #winestreamproxy = callPackage ./winestreamproxy { wine = wine-tkg; };
+
+      wine-ge = wineBuilder "wine-ge" "full" {};
+
+      wine-osu = wineBuilder "wine-osu" "base" {};
+
+      wine-tkg = wineBuilder "wine-tkg" "full" {};
+
+      wineprefix-preparer = pkgs.callPackage ./wineprefix-preparer {inherit (config.packages) dxvk-w32 vkd3d-proton-w32 dxvk-w64 vkd3d-proton-w64;};
     };
-
-    technic-launcher = callPackage ./technic-launcher {};
-
-    vkd3d-proton = callPackage ./vkd3d-proton {inherit pins;};
-    vkd3d-proton-w32 = pkgs.pkgsCross.mingw32.callPackage ./vkd3d-proton {inherit pins;};
-    vkd3d-proton-w64 = pkgs.pkgsCross.mingwW64.callPackage ./vkd3d-proton {inherit pins;};
-
-    wine-discord-ipc-bridge = callPackage ./wine-discord-ipc-bridge {
-      inherit pins;
-      wine = wine-tkg;
-    };
-
-    # broken
-    #winestreamproxy = callPackage ./winestreamproxy { wine = wine-tkg; };
-
-    wine-ge = wineBuilder "wine-ge" "full" {};
-
-    wine-osu = wineBuilder "wine-osu" "base" {};
-
-    wine-tkg = wineBuilder "wine-tkg" "full" {};
-
-    wineprefix-preparer = callPackage ./wineprefix-preparer {inherit dxvk-w32 vkd3d-proton-w32 dxvk-w64 vkd3d-proton-w64;};
   };
-in
-  packages
+}
