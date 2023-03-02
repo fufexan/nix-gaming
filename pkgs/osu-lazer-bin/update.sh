d59c534258d8d9779a5497e5a6a0f4e9616365ce
Mihai Fufezan <fufexan@protonmail.com>
2023-03-02 16:31:20 +0200

    osu-lazer-bin: fix update script

--- pkgs/osu-lazer-bin/update.sh
+++ pkgs/osu-lazer-bin/update.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S nix shell nixpkgs#jq nixpkgs#moreutils -c bash
+#!/usr/bin/env -S nix shell nixpkgs#jq -c bash
 
 info="pkgs/osu-lazer-bin/info.json"
 version=$(jq -r '.pins.osu.version' npins/sources.json)
@@ -6,8 +6,11 @@ oldversion=$(jq -r '.version' "$info")
 url="https://github.com/ppy/osu/releases/download/${version}/osu.AppImage"
 
 if [ "$oldversion" != "$version" ]; then
-  nix store prefetch-file "$url" --json > "$info"
-  jq --arg version "$version" '.version = $version' "$info" | sponge "$info"
+  if output=$(nix store prefetch-file "$url" --json); then
+    jq --arg version "$version" '.version = $version' <<<"$output" >"$info"
+  else
+    echo "osu!lazer has a non-release update"
+  fi
 else
   echo "osu!lazer is up to date."
 fi
