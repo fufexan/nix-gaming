419c68789270cac66173a3f60d772fd8169121ba
chayleaf <chayleaf-git@pavluk.org>
2023-05-30 18:40:46 +0700

    faf-client: fix build reproducibility

--- pkgs/faf-client/ice-adapter.nix
+++ pkgs/faf-client/ice-adapter.nix
@@ -22,30 +22,22 @@ in let
       stdenvNoCC.mkDerivation {
         pname = "${pname}-deps";
         inherit version src;
+        init_deps = ./init-deps.gradle;
         gradle_lockfile = ./gradle-ice.lockfile;
+        buildscript_gradle_lockfile = ./buildscript-gradle-ice.lockfile;
         postPatch = ''
           cp $gradle_lockfile ice-adapter/gradle.lockfile
+          cp $buildscript_gradle_lockfile ice-adapter/buildscript-gradle.lockfile
         '';
         nativeBuildInputs = [gradle perl];
         preBuild = ''
           export GRADLE_USER_HOME=$(mktemp -d)
+          export TERM=dumb
           sed -i "s/-SNAPSHOT/latest.integration/g" ice-adapter/build.gradle
-          sed -i "s/mavenLocal()//g" ice-adapter/build.gradle
-          cat <<END >> ice-adapter/build.gradle
-          dependencyLocking {
-            lockAllConfigurations()
-          }
-          task downloadDependencies {
-            doLast {
-              configurations.findAll{it.canBeResolved}.each{it.resolve()}
-              buildscript.configurations.findAll{it.canBeResolved}.each{it.resolve()}
-            }
-          }
-          END
         '';
         buildPhase = ''
           runHook preBuild
-          gradle --no-daemon -PjavafxPlatform=${jfxPlatform} ${
+          gradle --no-daemon --init-script $init_deps -PjavafxPlatform=${jfxPlatform} ${
             if enableJfx
             then ""
             else "-PjavafxClasspath=compileOnly"
@@ -55,8 +47,9 @@ in let
         installPhase = ''
           find $GRADLE_USER_HOME/caches/modules-2 -type f -regex '.*\.\(jar\|pom\)' \
             | perl -pe 's#(.*/([^/]+)/([^/]+)/([^/]+)/[0-9a-f]{30,40}/([^/\s]+))$# ($x = $2) =~ tr|\.|/|; "install -Dm444 $1 \$out/$x/$3/$4/$5" #e' \
+            | sort \
             | sh
-          cp ice-adapter/gradle.lockfile $out
+          cp ice-adapter/gradle.lockfile ice-adapter/buildscript-gradle.lockfile $out
         '';
         # couldn't figure out how to disable fixup...
         postFixup = ''
@@ -64,22 +57,23 @@ in let
           rmdir $out/share
         '';
         outputHashMode = "recursive";
-        outputHash = "sha256-UTlbZYRxoJWZyVt0zRkaQPD9d2UkKfbYmTqCCBy77Wg=";
+        outputHash = "sha256-xvyW9GdMfOyXdujodSK8T7d898pE4poYM/4d6KIcUFI=";
         passthru.updateLockfile = deps.overrideAttrs (old: {
           gradle_lockfile = "";
+          buildscript_gradle_lockfile = "";
           postPatch = "";
           # sadly, we have to do it twice to make sure the hashes match
           # (we have to download more pom files than we will need at build time before we can generate the lockfile)
           buildPhase = ''
             runHook preBuild
-            gradle --write-locks --no-daemon -PjavafxPlatform=${jfxPlatform} ${
+            gradle --write-locks --no-daemon --init-script $init_deps -PjavafxPlatform=${jfxPlatform} ${
               if enableJfx
               then ""
               else "-PjavafxClasspath=compileOnly"
             } :ice-adapter:downloadDependencies
             rm -rf $GRADLE_USER_HOME
             export GRADLE_USER_HOME=$(mktemp -d)
-            gradle --no-daemon -PjavafxPlatform=${jfxPlatform} ${
+            gradle --no-daemon --init-script $init_deps -PjavafxPlatform=${jfxPlatform} ${
               if enableJfx
               then ""
               else "-PjavafxClasspath=compileOnly"
@@ -118,8 +112,8 @@ in
     name = "${pname}-${version}.jar";
 
     postPatch = ''
-      cp ${deps}/gradle.lockfile ice-adapter/gradle.lockfile
-      chmod +w ice-adapter/gradle.lockfile
+      cp ${deps}/gradle.lockfile ${deps}/buildscript-gradle.lockfile ice-adapter/
+      chmod +w ice-adapter/gradle.lockfile ice-adapter/buildscript-gradle.lockfile
       sed -i "s/-SNAPSHOT/latest.integration/g" ice-adapter/build.gradle
     '';
 
