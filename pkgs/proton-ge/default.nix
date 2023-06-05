993b5e861a481d7245e8597759546246a536bf6e
NotAShelf <itsashelf@gmail.com>
2023-06-05 22:59:48 +0300

    proton-ge: 8.3 -> 8.4

--- pkgs/proton-ge/default.nix
+++ pkgs/proton-ge/default.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "proton-ge-custom";
-  version = "GE-Proton8-3";
+  version = "GE-Proton8-4";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-    hash = "sha256-JYGwb0LhIs6B2/OHiU+mJ/dAAS+Dg+MrVksAsn6IS9g=";
+    hash = "sha256-OPwmVxBGaWo51pDJcqvxvZ8qxMH8X0DwZTpwiKbdx/I=";
   };
 
   buildCommand = ''
@@ -21,7 +21,7 @@ stdenv.mkDerivation (finalAttrs: {
     description = "Compatibility tool for Steam Play based on Wine and additional components";
     homepage = "https://github.com/GloriousEggroll/proton-ge-custom";
     license = licenses.bsd3;
-    maintainers = with maintainers; [notashelf];
+    maintainers = with maintainers; [NotAShelf];
     platforms = ["x86_64-linux"];
   };
 })
