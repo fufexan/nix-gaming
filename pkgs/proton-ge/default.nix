4acf91ea6682af0dec7bcd241a84b243f8d93bb8
NotAShelf <raf@notashelf.dev>
2023-08-20 21:59:47 +0300

    proton-ge: 8.11 -> 8.12

--- pkgs/proton-ge/default.nix
+++ pkgs/proton-ge/default.nix
@@ -2,20 +2,25 @@
   stdenv,
   lib,
   fetchurl,
+  nix-update-script,
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "proton-ge-custom";
-  version = "GE-Proton8-11";
+  version = "GE-Proton8-12";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-    hash = "sha256-ldJBwp/9Cjb5k9FBrc6iz4/rnMhU5Ayf35+kNTAvXCg=";
+    hash = "sha256-9ZGuHCh5yISVOr8ggo7xRtWk+MWiGY7rIEXBLiBu48M=";
   };
 
   buildCommand = ''
+    runHook preBuild
     mkdir -p $out/bin
     tar -C $out/bin --strip=1 -x -f $src
+    runHook postBuild
   '';
+
+  passthru.updateScript = nix-update-script {};
 
   meta = with lib; {
     description = "Compatibility tool for Steam Play based on Wine and additional components";
