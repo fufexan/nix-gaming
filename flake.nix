ce450e9d3caa9aadf477184de427124331583acb
NotAShelf <raf@notashelf.dev>
2023-11-03 11:20:15 +0300

    flake: list available modules in throw if default module is used

--- flake.nix
+++ flake.nix
@@ -6,15 +6,19 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs:
+  outputs = {self, ...} @ inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      flake.nixosModules = {
+      flake.nixosModules = let
+        inherit (inputs.nixpkgs) lib;
+      in {
         pipewireLowLatency = import ./modules/pipewireLowLatency.nix;
         steamCompat = import ./modules/steamCompat.nix;
-        default = throw ''
+        default = throw (lib.mdDoc ''
           The usage of default module is deprecated as multiple modules are provided by nix-gaming. Please use
-          the exact name of the module you would like to use.
-        '';
+          the exact name of the module you would like to use. Available modules are:
+
+          ${builtins.concatStringsSep "\n" (lib.filter (name: name != "default") (lib.attrNames self.nixosModules))}
+        '');
       };
 
       imports = [
