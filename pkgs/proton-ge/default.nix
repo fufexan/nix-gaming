202b759bee4c413212e448f63f2805ae177aa7a0
NotAShelf <raf@notashelf.dev>
2023-11-19 19:30:43 +0300

    proton-ge: 8.22 -> 8.23

--- pkgs/proton-ge/default.nix
+++ pkgs/proton-ge/default.nix
@@ -6,11 +6,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "proton-ge-custom";
-  version = "GE-Proton8-22";
+  version = "GE-Proton8-23";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
-    hash = "sha256-JBS1CFdiOCKLWwavx/o+TFHUPFAA/wygrFcyO9SK9cc=";
+    hash = "sha256-gi+oDJSmA3hb70vvtr6KAq5Ri3SM9Is33sPX9DAFu/Y=";
   };
 
   buildCommand = ''
