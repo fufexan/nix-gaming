81af8b6232ed99c46413ca6d43aeb185c53f65b8
Mihai Fufezan <fufexan@protonmail.com>
2023-11-30 18:58:06 +0200

    Update packages

--- pkgs/faf-client/bin.nix
+++ pkgs/faf-client/bin.nix
@@ -24,8 +24,8 @@
     sha256 = sha256Stable;
   };
 
-  versionUnstable = "2023.12.0-snapshot";
-  sha256Unstable = "12nvbjd46d4kp41ar3d5xy282hmxv5jrbwiby0f6dxwnp0aql5lp";
+  versionUnstable = "2023.12.0-alpha-1";
+  sha256Unstable = "1ijvz9ksyl5w7gw15iqkg4dian7cg5lgqbhdzwwcb8yradv9jvmy";
   srcUnstable = builtins.fetchTarball {
     url = "https://github.com/FAForever/downlords-faf-client/releases/download/v${versionUnstable}/faf_unix_${builtins.replaceStrings ["."] ["_"] versionUnstable}.tar.gz";
     sha256 = sha256Unstable;
