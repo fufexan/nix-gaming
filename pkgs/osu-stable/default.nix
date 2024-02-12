372526e4ac64ebe4a1fb9b5373f7830c8be6cbe6
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2024-02-12 14:44:19 +0800

    osu-mime: rename icons to "osu!mime"

--- pkgs/osu-stable/default.nix
+++ pkgs/osu-stable/default.nix
@@ -62,7 +62,7 @@
   desktopItems = makeDesktopItem {
     name = pname;
     exec = "${script}/bin/${pname} %U";
-    icon = "osu!"; # icon comes from the osu-mime package
+    icon = "osu!mime"; # icon comes from the osu-mime package
     comment = "Rhythm is just a *click* away";
     desktopName = "osu!stable";
     categories = ["Game"];
