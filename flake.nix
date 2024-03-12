ec157f612a3849a3cb8ff6bc15f7c89de95a9b95
NotAShelf <raf@notashelf.dev>
2024-03-12 17:30:41 +0300

    modules: throw when `nixosModules.steamCompat` is imported

--- flake.nix
+++ flake.nix
@@ -18,7 +18,15 @@
         inherit (inputs.nixpkgs) lib;
       in {
         pipewireLowLatency = import ./modules/pipewireLowLatency.nix;
-        steamCompat = import ./modules/steamCompat.nix;
+
+        steamCompat = throw ''
+          nix-gaming.nixosModules.steamCompat is deprecated as of 2024-03-12 due to the addition of
+          `programs.steam.extraCompatPackages` to nixpkgs.
+
+          See https://github.com/NixOS/nixpkgs/pull/293564 for more details. You may remove the steamCompat
+          module to supress this message.
+        '';
+
         default = throw ''
           The usage of default module is deprecated as multiple modules are provided by nix-gaming. Please use
           the exact name of the module you would like to use. Available modules are:
