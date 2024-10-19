ed6e22752d8444a2bcf5d2a62cbcb447206ed748
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2024-10-19 17:17:03 +0700

    Add proton-osu package (temporary)

---
+++ pkgs/proton-osu/README.md
@@ -0,0 +1,4 @@
+# proton-osu
+
+Build of Wine/Proton with various patches, such as low-latency audio.
+Intended to be used with osu!.
---
+++ pkgs/proton-osu/default.nix
@@ -0,0 +1,41 @@
+{
+  lib,
+  stdenvNoCC,
+  fetchzip
+}:
+
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "proton-osu-bin";
+  version = "proton-osu-9-7";
+
+  src = fetchzip {
+    url = "https://github.com/whrvt/umubuilder/releases/download/${finalAttrs.version}/${finalAttrs.version}.tar.gz";
+    hash = lib.fakeHash;
+  };
+
+  outputs = [
+    "out"
+    "steamcompattool"
+  ];
+
+  buildCommand = ''
+    runHook preBuild
+
+    echo "${finalAttrs.pname} should not be installed into environments. Please use programs.steam.extraCompatPackages instead." > $out
+
+    ln -s $src $steamcompattool
+
+    runHook postBuild
+  '';
+
+  meta = {
+    description = ''
+      Compatibily tool for Steam Play, patched with low-latency audio and intended for osu!.
+
+      (This is intended for use in the `programs.steam.extraCompatPackages` option only.)
+    '';
+    homepage = "https://github.com/whrvt/umubuilder";
+    license = lib.licenses.gpl3plus;
+    platforms = [ "x86_64-linux" ];
+  };
+})
