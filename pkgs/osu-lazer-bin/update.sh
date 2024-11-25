793d14922677ddfc4321bd9fde633e10fa6f74e7
ProspectPyxis <ProspectPyxis@users.noreply.github.com>
2024-11-25 08:01:15 +0700

    Remove osu!lazer update script npins dependency

--- pkgs/osu-lazer-bin/update.sh
+++ pkgs/osu-lazer-bin/update.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env -S nix shell nixpkgs#jq -c bash
+#!/usr/bin/env -S nix shell nixpkgs#curl nixpkgs#jq -c bash
 
 info="pkgs/osu-lazer-bin/info.json"
-version=$(jq -r '.pins.osu.version' npins/sources.json)
+version=$(curl -s "https://api.github.com/repos/ppy/osu/releases/latest" | jq -r '.tag_name')
 oldversion=$(jq -r '.version' "$info")
 url="https://github.com/ppy/osu/releases/download/${version}/osu.AppImage"
 
@@ -9,7 +9,7 @@ if [ "$oldversion" != "$version" ]; then
   if output=$(nix store prefetch-file "$url" --json); then
     jq --arg version "$version" '.version = $version' <<<"$output" >"$info"
   else
-    echo "osu!lazer has a non-release update"
+    echo "Failed to fetch new osu!lazer binary, possibly non-release update"
   fi
 else
   echo "osu!lazer is up to date."
