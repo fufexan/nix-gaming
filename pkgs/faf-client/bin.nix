f0bba32370e38a6ed18d7c5c741d23fe53e2d265
Mihai Fufezan <mihai@fufexan.net>
2025-01-12 15:48:15 +0200

    Update packages

--- pkgs/faf-client/bin.nix
+++ pkgs/faf-client/bin.nix
@@ -19,15 +19,15 @@
 }: let
   pname = "faf-client-bin";
 
-  versionStable = "2024.10.0";
-  sha256Stable = "0lrdx59fhkwcdz3n8b25nypzxbhk1mdnl7f8i9k1yjryprm4fd11";
+  versionStable = "2025.1.0";
+  sha256Stable = "0a7r9hyg3r9ndxy9qyyqh4hl8nsg3p9i2380kdzpvbaqyg1gwv7i";
   srcStable = builtins.fetchTarball {
     url = "https://github.com/FAForever/downlords-faf-client/releases/download/v${versionStable}/faf_unix_${builtins.replaceStrings ["."] ["_"] versionStable}.tar.gz";
     sha256 = sha256Stable;
   };
 
-  versionUnstable = "2024.10.0";
-  sha256Unstable = "0lrdx59fhkwcdz3n8b25nypzxbhk1mdnl7f8i9k1yjryprm4fd11";
+  versionUnstable = "2025.1.0";
+  sha256Unstable = "0a7r9hyg3r9ndxy9qyyqh4hl8nsg3p9i2380kdzpvbaqyg1gwv7i";
   srcUnstable = builtins.fetchTarball {
     url = "https://github.com/FAForever/downlords-faf-client/releases/download/v${versionUnstable}/faf_unix_${builtins.replaceStrings ["."] ["_"] versionUnstable}.tar.gz";
     sha256 = sha256Unstable;
