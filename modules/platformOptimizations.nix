a6a75391a8ce54244da7a81854473613dd8892cf
Melody Renata <me@lovingmelody.io>
2025-02-06 22:34:49 -0500

    steam.platformOptimizations: Update steamos customizations
    Update platformOptimizations to match changes upstream
    source: https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steamos-customizations-jupiter-20250117.1-1-any.pkg.tar.zst

--- modules/platformOptimizations.nix
+++ modules/platformOptimizations.nix
@@ -10,7 +10,7 @@
   };
 
   config = lib.mkIf config.programs.steam.platformOptimizations.enable {
-    # last cheched with https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steamos-customizations-jupiter-20240219.1-2-any.pkg.tar.zst
+    # last checked with https://steamdeck-packages.steamos.cloud/archlinux-mirror/jupiter-main/os/x86_64/steamos-customizations-jupiter-20250117.1-1-any.pkg.tar.zst
     boot.kernel.sysctl = {
       # 20-shed.conf
       "kernel.sched_cfs_bandwidth_slice_us" = 3000;
@@ -18,6 +18,10 @@
       # This is required due to some games being unable to reuse their TCP ports
       # if they're killed and restarted quickly - the default timeout is too large.
       "net.ipv4.tcp_fin_timeout" = 5;
+      # 30-splitlock.conf
+      # Prevents intentional slowdowns in case games experience split locks
+      # This is valid for kernels v6.0+
+      "kernel.split_lock_mitigate" = 0;
       # 30-vm.conf
       # USE MAX_INT - MAPCOUNT_ELF_CORE_MARGIN.
       # see comment in include/linux/mm.h in the kernel tree.
