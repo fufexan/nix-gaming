da95abea42f0517033bb41e45035f0b0cf348d9b
Luna Nova <git@lunnova.dev>
2025-05-19 17:32:44 -0700

    wineprefix-preparer: handle mcfgthread changes, fix syswow64 path on wine 10+

--- pkgs/wineprefix-preparer/default.nix
+++ pkgs/wineprefix-preparer/default.nix
@@ -3,6 +3,7 @@
   dxvk-w32,
   vkd3d-proton-w64,
   vkd3d-proton-w32,
+  pkgsCross,
   writeShellScriptBin,
 }:
 # This installer is completely custom rather than using upstream scripts.
@@ -17,26 +18,27 @@ writeShellScriptBin "wineprefix-preparer"
   export WINEPREFIX="''${WINEPREFIX:-$HOME/.wine}"
   echo "Preparing prefix $WINEPREFIX for gaming"
 
-  set -euo pipefail
+  set -xeuo pipefail
 
   echo "Killing running wine processes/wineserver"
   wineserver -k || true
 
   echo "Running wineboot -u to update prefix"
-  WINEDEBUG=-all wineboot -u; sleep 1
+  WINEDEBUG=-all wineboot -u || :; sleep 1
 
   echo "Stopping processes in session"
   wineserver -k || true
 
-  win64_sys_path=$(wine64 winepath -u 'C:\windows\system32' 2> /dev/null)
+  echo "Finding paths"
+  win64_sys_path=$(WINEARCH=win64 wine winepath -u 'C:\windows\system32' 2> /dev/null)
   win64_sys_path="''${win64_sys_path/$'\r'/}"
-  win32_sys_path=$(wine winepath -u 'C:\windows\system32' 2> /dev/null)
+  win32_sys_path=$(wine winepath -u 'C:\windows\syswow64' 2> /dev/null)
   win32_sys_path="''${win32_sys_path/$'\r'/}"
 
   echo "Found 32 bit path $win32_sys_path and 64 bit path $win64_sys_path"
 
   echo "Removing existing dxvk and vkd3d-proton DLLs"
-  rm -rf {"$win32_sys_path","$win64_sys_path"}/{dxgi,d3d9,d3d10core,d3d11,d3d12}.dll
+  rm -rf {"$win32_sys_path","$win64_sys_path"}/{dxgi,d3d9,d3d10core,d3d11,d3d12,d3d12core,mcfgthread-12}.dll
 
   echo "Installing dxvk DLLs"
   install -v -D -m644 -t "$win64_sys_path" ${dxvk-w64}/bin/*.dll
@@ -47,7 +49,7 @@ writeShellScriptBin "wineprefix-preparer"
   install -v -D -m644 -t "$win32_sys_path" ${vkd3d-proton-w32}/bin/*.dll
 
   echo "Adding native DllOverrides"
-  for dll in dxgi d3d9 d3d10core d3d11 d3d12; do
+  for dll in dxgi d3d9 d3d10core d3d11 d3d12 d3d12core; do
     wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v $dll /d native /f >/dev/null 2>&1
   done
 ''
