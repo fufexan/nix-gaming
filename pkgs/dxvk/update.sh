aa3d601b4c5f8037fdb9fd9cdf4e5cebbcd3578d
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-07 17:31:17 +0800

    dxvk: add an update script to fetch dxvk source based on the version of gplasync patch

---
+++ pkgs/dxvk/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S nix shell nixpkgs#npins nixpkgs#jq -c bash
+
+NAME="dxvk"
+
+REPO_OWNER="doitsujin"
+REPO_NAME="dxvk"
+
+GPLASYNC_VERSION=$(jq -r '.pins."dxvk-gplasync".version' npins/sources.json)
+VERSION="${GPLASYNC_VERSION%%-*}"
+
+npins add github --frozen --submodules --name "$NAME" --at "$VERSION" "$REPO_OWNER" "$REPO_NAME"
