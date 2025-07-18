c674b26b7cb866d2b4e171a562cdd6e4adeeb1d0
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-19 03:01:22 +0800

    wine-tkg{,ntsync}: build with default gcc

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -28,7 +28,7 @@
       wineBuilder = wine: build: extra:
         (import ./wine ({
             inherit inputs self pkgs build pins wine-mono;
-            inherit (pkgs) callPackage fetchFromGitHub fetchurl lib moltenvk pkgsCross pkgsi686Linux stdenv_32bit stdenv wrapCCMulti overrideCC gcc13;
+            inherit (pkgs) callPackage fetchFromGitHub lib moltenvk pkgsCross pkgsi686Linux gccMultiStdenv stdenv wrapCCMulti overrideCC gcc13;
             supportFlags = (import ./wine/supportFlags.nix).${build};
           }
           // extra))
