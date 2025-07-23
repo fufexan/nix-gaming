c001d00ce7e279bcf35717e2565236521610e260
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-24 00:38:02 +0800

    ci: add update script for wine-cachyos

--- pkgs/wine/update-wine-cachyos.sh
+++ pkgs/wine/update-wine-cachyos.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S nix shell .#npins -c bash
 
-REPO_OWNER="GloriousEggroll"
-REPO_NAME="proton-wine"
+REPO_OWNER="CachyOS"
+REPO_NAME="wine-cachyos"
 
 # Get branch names
 branches=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches?per_page=10000" | grep -oP '(?<="name": ")[^"]+')
@@ -9,15 +9,18 @@ branches=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches
 # Initialize variables
 latest_major=0
 latest_minor=0
+latest_date=0
 
 # Find the latest version
 for branch in $branches; do
-  if [[ $branch =~ ^Proton([0-9]+)-([0-9]+)$ ]]; then
+  if [[ $branch =~ ^cachyos_([0-9]+)\.([0-9]+)_([0-9]+)/main$ ]]; then
     major=${BASH_REMATCH[1]}
     minor=${BASH_REMATCH[2]}
-    if (( major > latest_major )) || (( major == latest_major && minor > latest_minor )); then
+    date=${BASH_REMATCH[3]}
+    if (( major > latest_major )) || (( major == latest_major && minor > latest_minor )) || (( major == latest_major && minor == latest_minor && date > latest_date )); then
       latest_major=$major
       latest_minor=$minor
+      latest_date=$date
       latest_branch=$branch
     fi
   fi
