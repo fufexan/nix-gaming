6737e04e3973d16e290011c117bf1b6d6d118a12
ccicnce113424 <ccicnce113424@gmail.com>
2025-07-25 14:11:59 +0800

    ci: refactor update scripts using jq

--- pkgs/wine/update-wine-cachyos.sh
+++ pkgs/wine/update-wine-cachyos.sh
@@ -1,29 +1,23 @@
-#!/usr/bin/env -S nix shell .#npins -c bash
+#!/usr/bin/env -S nix shell .#npins nixpkgs#jq -c bash
 
 REPO_OWNER="CachyOS"
 REPO_NAME="wine-cachyos"
 
-# Get branch names
-branches=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches?per_page=10000" | grep -oP '(?<="name": ")[^"]+')
-
-# Initialize variables
-latest_major=0
-latest_minor=0
-latest_date=0
-
-# Find the latest version
-for branch in $branches; do
-  if [[ $branch =~ ^cachyos_([0-9]+)\.([0-9]+)_([0-9]+)/main$ ]]; then
-    major=${BASH_REMATCH[1]}
-    minor=${BASH_REMATCH[2]}
-    date=${BASH_REMATCH[3]}
-    if (( major > latest_major )) || (( major == latest_major && minor > latest_minor )) || (( major == latest_major && minor == latest_minor && date > latest_date )); then
-      latest_major=$major
-      latest_minor=$minor
-      latest_date=$date
-      latest_branch=$branch
-    fi
-  fi
-done
+# Get the latest branch using jq
+latest_branch=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches?per_page=10000" | \
+  jq -r '
+    [.[] |
+     (.name | capture("^cachyos_(?<major>[0-9]+)\\.(?<minor>[0-9]+)_(?<date>[0-9]+)/main$")) as $parts |
+     select($parts) |
+     {
+       name: .name,
+       major: ($parts.major | tonumber),
+       minor: ($parts.minor | tonumber),
+       date: ($parts.date | tonumber)
+     }
+    ] |
+    max_by([.major, .minor, .date]) |
+    .name
+  ')
 
 npins add github --frozen -b "$latest_branch" "$REPO_OWNER" "$REPO_NAME"
--- pkgs/wine/update-wine-ge.sh
+++ pkgs/wine/update-wine-ge.sh
@@ -1,26 +1,22 @@
-#!/usr/bin/env -S nix shell .#npins -c bash
+#!/usr/bin/env -S nix shell .#npins nixpkgs#jq -c bash
 
 REPO_OWNER="GloriousEggroll"
 REPO_NAME="proton-wine"
 
-# Get branch names
-branches=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches?per_page=10000" | grep -oP '(?<="name": ")[^"]+')
-
-# Initialize variables
-latest_major=0
-latest_minor=0
-
-# Find the latest version
-for branch in $branches; do
-  if [[ $branch =~ ^Proton([0-9]+)-([0-9]+)$ ]]; then
-    major=${BASH_REMATCH[1]}
-    minor=${BASH_REMATCH[2]}
-    if (( major > latest_major )) || (( major == latest_major && minor > latest_minor )); then
-      latest_major=$major
-      latest_minor=$minor
-      latest_branch=$branch
-    fi
-  fi
-done
+# Get the latest branch using jq
+latest_branch=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/branches?per_page=10000" | \
+  jq -r '
+    [.[] |
+     (.name | capture("^Proton(?<major>[0-9]+)-(?<minor>[0-9]+)$")) as $parts |
+     select($parts) |
+     {
+       name: .name,
+       major: ($parts.major | tonumber),
+       minor: ($parts.minor | tonumber)
+     }
+    ] |
+    max_by([.major, .minor]) |
+    .name
+  ')
 
 npins add github --frozen -b "$latest_branch" "$REPO_OWNER" "$REPO_NAME"
