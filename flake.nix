e49370e1f20abd455e8c4ee72799ec81c7d11126
Mihai Fufezan <mihai@fufexan.net>
2026-02-23 10:25:15 +0200

    flake: alejandra -> nixfmt-tree
    Add git-hooks

--- flake.nix
+++ flake.nix
@@ -4,28 +4,50 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {self, ...} @ inputs:
-    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    { self, ... }@inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./lib
         ./modules
         ./pkgs
         ./tests
+        ./fmt-hooks.nix
       ];
 
-      perSystem = {pkgs, ...}: {
-        formatter = pkgs.alejandra;
-        # Add to legacyPackages to prevent easyOverlay from including this package in the Overlay.
-        legacyPackages.npins = pkgs.callPackage (let pins = import ./npins; in pins.npins + "/npins.nix") {};
-      };
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-tree;
+          # Add to legacyPackages to prevent easyOverlay from including this package in the Overlay.
+          legacyPackages.npins = pkgs.callPackage (
+            let
+              pins = import ./npins;
+            in
+            pins.npins + "/npins.nix"
+          ) { };
+
+          devShells.default = pkgs.mkShell {
+            shellHook = ''
+              ${config.pre-commit.installationScript}
+            '';
+          };
+        };
     };
 
   # auto-fetch deps when `nix run/shell`ing
   nixConfig = {
     allowInsecure = true;
-    extra-substituters = ["https://nix-gaming.cachix.org"];
-    extra-trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+    extra-substituters = [ "https://nix-gaming.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
   };
 }
